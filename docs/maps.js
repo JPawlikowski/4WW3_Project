7a53f8ec0b87ceb77543df1cb687f090238843c3
Anthony Mella <tony@yeezymaster.local>
2019-10-17 16:55:27 -0400

    map js changes

--- docs/maps.js
+++ docs/maps.js
@@ -1,8 +1,18 @@
 var map;
+var current_map;
 
-function initMap() {
+function show_map() {
     map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
     });
+}
+
+//displays the users current position
+function current_position(elementid) {
+    current_map = new google.maps.Map(document.getElementById(elementid), {
+        center: {},
+        zoom: 8
+    });
+
 }
---
+++ validate_submission.js
@@ -0,0 +1,16 @@
+const name_regex = ;
+const address_reges = ;
+
+// Will the picture be optional or do we have to validate that?
+
+function validate_address() {
+    var address = document.getElementById("address").value;
+}
+
+function valiate_name() {
+    var name = document.getElementById("name").value;
+}
+
+function validate_rating() {
+
+}
