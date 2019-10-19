5468394055a1f14b1a8c203cfe2e307c1d3550bc
Anthony Mella <tony@yeezymaster.local>
2019-10-19 11:58:34 -0400

    moved validate_submission to docs

---
+++ docs/validate_submission.js
@@ -0,0 +1,16 @@
+const name_regex = "h";
+const address_reges = "hh";
+
+// Will the picture be optional or do we have to validate that?
+
+function validate_address() {
+    var address = document.getElementById("address").value;
+}
+
+function valiate_name() {
+    var name = document.getElementById("name").value;
+}
+
+function validate_rating() {
+
+}
