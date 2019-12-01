8924e26e786614ef1b5ec0b9d9a58749e3b4a625
Jakub Pawlikowski <pawlikoj@mcmaster.ca>
2019-12-01 13:32:11 -0500

    s3 related

---
+++ docs/s3upload.php
@@ -0,0 +1,51 @@
+<?PHP
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+
+$bucketName = '4ww3a3';
+$IAM_KEY = 'AKIA2BUVB2TUHYZGAXZS';
+$IAM_SECRET = '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';
+
+try {
+		// You may need to change the region. It will say in the URL when the bucket is open
+		// and on creation.
+		$s3 = S3Client::factory(
+			array(
+				'credentials' => array(
+					'key' => $IAM_KEY,
+					'secret' => $IAM_SECRET
+				),
+				'version' => 'latest',
+				'region'  => 'us-east-2'
+			)
+		);
+	} catch (Exception $e) {
+		// We use a die, so if this fails. It stops here. Typically this is a REST call so this would
+		// return a json object.
+		die("Error: " . $e->getMessage());
+	}
+
+	$keyName = basename($_FILES["fileToUpload"]['name']);
+	$pathInS3 = 'https://s3.us-east-2.amazonaws.com/' . $bucketName . '/' . $keyName;
+
+	try {
+		// Uploaded:
+		$file = $_FILES["fileToUpload"]['tmp_name'];
+		$s3->putObject(
+			array(
+				'Bucket'=>$bucketName,
+				'Key' =>  $keyName,
+				'SourceFile' => $file,
+				'StorageClass' => 'REDUCED_REDUNDANCY'
+			)
+		);
+	} catch (S3Exception $e) {
+		die('Error:' . $e->getMessage());
+	} catch (Exception $e) {
+		die('Error:' . $e->getMessage());
+	}
+	echo 'Done';
+
+?>
